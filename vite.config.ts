fec48497bf898c9ee957fa0e4f370da1f65a8317
Ronit <r62544400@gmail.com>
2026-09-17 19:05:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+   base: '/Ladies-First-Beauty-Salon/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
